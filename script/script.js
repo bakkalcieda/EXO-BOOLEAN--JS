f710210e8ff8e1d5ddf02e8804ac7164e5b90ad2
BAKKALCI Eda <stg482043@stg-bf.be>
2026-05-19 10:48:46 +0200

    master

---
+++ script/script.js
@@ -0,0 +1,31 @@
+const a = 13;
+const b = 22;
+const c = 2;
+const d = 42;
+const e = 21;
+const f = true;
+const g = 26;
+const h = 3.14;
+const i = '42';
+const k = 'Six';
+const name1 = 'Riri';
+const name2 = 'Fifi';
+const name3 = 'Zaza';
+const name4 = 'Loulou';
+
+// Trouvez les resultats pour :
+/*
+ 1) a > b
+ 2) (a + c) < b
+ 3) !(b > e)
+ 4) c > (b + e)
+ 5) (a * 2) >= g
+ 6) f && (a <= b) && (d/2 === e)
+ 7) (h * 2) > 6
+ 8) Math.floor(e / 2) > (g / a)
+ 9) (name1 > name2) && (name3 > name4)
+10) (name1 > 'Donald') || (name4 > 'Balthazar')
+11) (a > c) && !(e > g) && (a % 2 === 0) && !f
+12) ((d === (e * 2)) || (e > g)) && (d === i)
+13) ((a * 2) > (g / 2)) || f || ((g + b + c) > (d * h))
+14) k > 5 */
